c48b6d17164c61213f4d07acbcf7b141e049d623
K3CSK3 <wiattila8@gmail.com>
2022-10-27 07:52:50 +0200

    Ora-okt-27

---
+++ 05 - Elágazások (if-else)/Feladat 06/Main.py
@@ -0,0 +1,12 @@
+numberOne :int = None
+numberTwo :int = None
+numberThree :int = None
+
+print("Adjon meg egy számot",end='')
+numberOne = int(input())
+
+print("Adjon még egy számot",end='')
+numberTwo = int(input())
+
+print("Adjon egy harmadik számot",end='')
+numberThree = int(input())
