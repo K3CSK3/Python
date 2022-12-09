303397ab7ba45d52fce964700c99500c962385c9
K3CSK3 <wiattila8@gmail.com>
2022-12-09 07:53:12 +0100

    Ora-dec-09

--- 07 - For Ciklus/Feladat Piramis/MainEredeti.py
+++ 07 - For Ciklus/Feladat Piramis/MainEredeti.py
@@ -4,5 +4,5 @@ lista :list = [1]
 print("Kérem adja meg hány elemű a számpiramis?")
 sor = int(input())
 for i in range(1, sor+1):
-    print(' '.join(map(str, lista)))
-    lista.append(f"  {i+1}  ")
+    print('  '.join(map(str, lista)))
+    lista.append(f" {i+1} ")
--- 07 - For Ciklus/Feladat Piramis/MainSzimetrikus.py
+++ 07 - For Ciklus/Feladat Piramis/MainSzimetrikus.py
@@ -3,9 +3,7 @@ lista :list = []
 
 print("Kérem adja meg hány elemű a számpiramis?")
 sor = int(input())
-for i in range(sor+1, 1, -1):
-    lista.append(f" {i-1} ")
-
-for i in range(sor, 0, -1):
+for i in range(0, sor+1, 2):
     print(' '.join(map(str, lista)))
-    del lista[0]
+    lista.append(f"  {i+1}  ")
+    lista.append(f"  {i+2}  ")
