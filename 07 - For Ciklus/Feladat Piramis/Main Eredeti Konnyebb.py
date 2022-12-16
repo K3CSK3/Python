040540b4852383ab0ed77a0b4cd5841d57218067
K3CSK3 <wiattila8@gmail.com>
2022-12-16 07:48:47 +0100

    Ora-dec-16

--- 07 - For Ciklus/Feladat Piramis/Main Eredeti Konnyebb.py
+++ 07 - For Ciklus/Feladat Piramis/Main Eredeti Konnyebb.py
@@ -1,13 +1,6 @@
 print("Kérem adja meg hány elemű a számpiramis?")
-szam = int(input())
-for i in range(0, szam):
-    for j in range(0, i+1):
-        print(f"{szam} ", end="")
-print()
-
-
-n = int(input("Enter the number of rows")) 
-for i in range(0, n): 
-    for j in range(0, i + 1): 
-        print(f"{n} ", end="") 
+szam = int(input()) 
+for i in range(0, szam+1): 
+    for j in range(1, i+1):
+        print(f"{j} ", end="") 
     print() 
--- 07 - For Ciklus/Feladat Piramis/MainSzimetrikus.py
+++ 07 - For Ciklus/Feladat Piramis/MainSzimetrikus.py
@@ -4,13 +4,13 @@ lista :list = []
 print("Kérem adja meg hány elemű a számpiramis?")
 sor = int(input())
 
-for i in range(1, sor+1, 1):
-    lista.append(f" ")
+for i in range(1, sor*2, 1):
+    lista.append("")
 lista.append(1)
 
-for i in range(1, sor+1, 2):
+for i in range(1, sor*2, 2):
     print(' '.join(map(str, lista)))
     del lista[0]
     del lista[0]
-    lista.append(f"  {i+1} ")
-    lista.append(f" {i+2}  ")
+    lista.append(f"{i+1}")
+    lista.append(f"{i+2}")
